465d8062e37d3edbdbbc4759b72fc3d6344a8307
Patryk <83609040+FuriaPaladins@users.noreply.github.com>
2025-02-10 17:42:56 +0000

    Add statue level to exploration stats

--- genshin/models/genshin/chronicle/stats.py
+++ genshin/models/genshin/chronicle/stats.py
@@ -116,6 +116,7 @@ class Exploration(APIModel):
     boss_list: typing.Sequence[BossKill]
     area_exploration_list: typing.Sequence[AreaExploration]
     natlan_reputation: typing.Optional[NatlanReputation] = Aliased("natan_reputation", default=None)
+    statue_level: int = Aliased("seven_statue_level", None)
 
     @property
     def explored(self) -> float:
