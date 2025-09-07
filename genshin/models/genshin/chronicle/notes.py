3c7a68b72ec53bc112ac8df06d152312962a2552
Furia <83609040+FuriaPaladins@users.noreply.github.com>
2025-09-07 02:38:58 +0100

    Fix Stored Attendance Refresh Cooldown

--- genshin/models/genshin/chronicle/notes.py
+++ genshin/models/genshin/chronicle/notes.py
@@ -134,9 +134,7 @@ class DailyTasks(APIModel):
     attendance_visible: bool
 
     stored_attendance: float
-    stored_attendance_refresh_countdown: typing.Optional[datetime.timedelta] = Aliased(
-        "attendance_refresh_time", default=None
-    )
+    stored_attendance_refresh_countdown: typing.Optional[datetime.timedelta] = None
 
 
 class ArchonQuestStatus(str, enum.Enum):
